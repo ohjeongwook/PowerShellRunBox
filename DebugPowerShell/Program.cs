40bc708c84ece7a5db196a77933dc0787fcb2e37
Jeong Wook Oh <oh.jeongwook@gmail.com>
2019-09-26 13:08:17 -0700

    Removed default banner

--- DebugPowerShell/Program.cs
+++ DebugPowerShell/Program.cs
@@ -8,14 +8,6 @@ namespace DebugPowerShell
     {
         private static void Main(string[] args)
         {
-            Console.Title = "PowerShell Debugger Sample Application";
-            Logger.Out("    Windows PowerShell Debugger Application Sample");
-            Logger.Out("    ==============================================");
-            Logger.Out();
-            Logger.Out("This is a simple example of using the PowerShell Debugger API to set");
-            Logger.Out("breakpoints in a script file, run the script and handle debugger events.");
-            Logger.Out();
-
             if (args.Count() < 1)
             {
                 return;
