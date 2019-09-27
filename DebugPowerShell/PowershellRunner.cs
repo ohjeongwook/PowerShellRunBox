bda626359833fd12880f35ed85dd94dea5f006f6
Jeong Wook Oh <oh.jeongwook@gmail.com>
2019-09-27 10:39:28 -0700

    Improved variable dump

--- DebugPowerShell/PowershellRunner.cs
+++ DebugPowerShell/PowershellRunner.cs
@@ -74,6 +74,123 @@ namespace DebugPowerShell
 
         private Dictionary<string, object> VariableMap = new Dictionary<string, object>();
 
+        public static string DumpHex(byte[] bytes, int bytesPerLine = 16, string prefix="")
+        {
+            if (bytes == null) return "<null>";
+            int bytesLength = bytes.Length;
+
+            char[] HexChars = "0123456789ABCDEF".ToCharArray();
+
+            int firstHexColumn =
+                  8                   // 8 characters for the address
+                + 3;                  // 3 spaces
+
+            int firstCharColumn = firstHexColumn
+                + bytesPerLine * 3       // - 2 digit for the hexadecimal value and 1 space
+                + (bytesPerLine - 1) / 8 // - 1 extra space every 8 characters from the 9th
+                + 2;                  // 2 spaces 
+
+            int lineLength = firstCharColumn
+                + bytesPerLine           // - characters to show the ascii value
+                + Environment.NewLine.Length; // Carriage return and line feed (should normally be 2)
+
+            char[] line = (new String(' ', lineLength - 2) + Environment.NewLine).ToCharArray();
+            int expectedLines = (bytesLength + bytesPerLine - 1) / bytesPerLine;
+            StringBuilder result = new StringBuilder(expectedLines * lineLength);
+
+            for (int i = 0; i < bytesLength; i += bytesPerLine)
+            {
+                line[0] = HexChars[(i >> 28) & 0xF];
+                line[1] = HexChars[(i >> 24) & 0xF];
+                line[2] = HexChars[(i >> 20) & 0xF];
+                line[3] = HexChars[(i >> 16) & 0xF];
+                line[4] = HexChars[(i >> 12) & 0xF];
+                line[5] = HexChars[(i >> 8) & 0xF];
+                line[6] = HexChars[(i >> 4) & 0xF];
+                line[7] = HexChars[(i >> 0) & 0xF];
+
+                int hexColumn = firstHexColumn;
+                int charColumn = firstCharColumn;
+
+                for (int j = 0; j < bytesPerLine; j++)
+                {
+                    if (j > 0 && (j & 7) == 0) hexColumn++;
+                    if (i + j >= bytesLength)
+                    {
+                        line[hexColumn] = ' ';
+                        line[hexColumn + 1] = ' ';
+                        line[charColumn] = ' ';
+                    }
+                    else
+                    {
+                        byte b = bytes[i + j];
+                        line[hexColumn] = HexChars[(b >> 4) & 0xF];
+                        line[hexColumn + 1] = HexChars[b & 0xF];
+                        line[charColumn] = asciiSymbol(b);
+                    }
+                    hexColumn += 3;
+                    charColumn++;
+                }
+                result.Append(prefix + new string(line));
+            }
+            return result.ToString();
+        }
+
+        static char asciiSymbol(byte val)
+        {
+            if (val < 32) return '.';  // Non-printable ASCII
+            if (val < 127) return (char)val;   // Normal ASCII
+            // Handle the hole in Latin-1
+            if (val == 127) return '.';
+            if (val < 0x90) return "€.‚ƒ„…†‡ˆ‰Š‹Œ.Ž."[val & 0xF];
+            if (val < 0xA0) return ".‘’“”•–—˜™š›œ.žŸ"[val & 0xF];
+            if (val == 0xAD) return '.';   // Soft hyphen: this symbol is zero-width even in monospace fonts
+            return (char)val;   // Normal Latin-1
+        }
+
+        private void DumpVariable(string name, object value, int level=0)
+        {
+            string prefix = "";
+            for (int i=0; i< level; i++)
+            {
+                prefix += "   ";
+            }
+
+            if (value is Array)
+            {
+                if (!string.IsNullOrEmpty(name))
+                {
+                    Logger.Out(prefix + @"* " + name);
+                }
+
+                Array arr = (Array)(value);
+
+                if (value is System.Char[])
+                {
+                    byte[] bytes = Encoding.ASCII.GetBytes((System.Char[])value);
+                    Logger.Out(DumpHex(bytes, 16, prefix + "   "));
+                }
+                else
+                {
+                    foreach (object element in (Array)(value))
+                    {
+                        DumpVariable("", element, level + 1);
+                    }
+                }
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(name))
+                {
+                    Logger.Out(prefix + value);
+                }
+                else
+                {
+                    Logger.Out(prefix + @"* " + name + ": " + value);
+                }
+            }
+        }
+
         private bool UpdateVariableMap(Debugger debugger)
         {
             bool updatedVariable = false;
@@ -94,14 +211,17 @@ namespace DebugPowerShell
                             )
                         )
                         {
-                            Logger.Out(@"* " + psv.Name + ": " + psv.Value);
+                            DumpVariable(psv.Name, psv.Value);
                         }
 
                         if (VariableReplaceMap != null && VariableReplaceMap.ContainsKey(psv.Name))
                         {
-                            Logger.Out(@"Updated variable " + psv.Name + ": " + psv.Value + " --> " + VariableReplaceMap[psv.Name]);
-                            psv.Value = VariableReplaceMap[psv.Name];
-                            updatedVariable = true;
+                            if (psv.Value != VariableReplaceMap[psv.Name])
+                            {
+                                Logger.Out(@"Updated variable " + psv.Name + ": " + psv.Value + " --> " + VariableReplaceMap[psv.Name]);
+                                psv.Value = VariableReplaceMap[psv.Name];
+                                updatedVariable = true;
+                            }
                         }
 
                         VariableMap[psv.Name] = psv.Value;
@@ -207,16 +327,9 @@ namespace DebugPowerShell
             Debugger debugger = sender as Debugger;
             DebuggerResumeAction? resumeAction = null;
 
-            PrintDebuggerStopMessage(args);
-            bool updatedVariable = UpdateVariableMap(debugger);
+            UpdateVariableMap(debugger);
 
-            if (updatedVariable)
-            {
-                args.ResumeAction = DebuggerResumeAction.Continue;
-                return;
-            }
-
-            foreach(Breakpoint breakpoint in args.Breakpoints)
+            foreach (Breakpoint breakpoint in args.Breakpoints)
             {
                 if (BreakPoints.ContainsKey(breakpoint.Id))
                 {
@@ -224,6 +337,7 @@ namespace DebugPowerShell
                 }
             }
 
+            PrintDebuggerStopMessage(args);
             while (resumeAction == null)
             {
                 if (CommandCount > 0)
