9c07922f0b232698f2672429511abfc3638eac1f
Jeong Wook Oh <oh.jeongwook@gmail.com>
2019-09-27 01:10:46 -0700

    Added support to update variable by variable breakpoint

--- DebugPowerShell/PowershellRunner.cs
+++ DebugPowerShell/PowershellRunner.cs
@@ -13,112 +13,76 @@ namespace DebugPowerShell
 {
     class PowershellRunner
     {
-        #region Public Methods
         public Dictionary<string, string> VariableReplaceMap { get; set; } = new Dictionary<string, string>();
-
-        public PowershellRunner(string fileName=null)
+        public PowershellRunner(string fileName = null)
         {
-            if ( fileName!=null )
+            if (fileName != null)
             {
-                LoadConfig(fileName);
-            }
-        }
-
-        public void RunCode(string[] args)
-        {
-            string filePath = string.Empty;
-
-            if (!System.IO.File.Exists(args[0]))
-            {
-                filePath = System.IO.Path.GetTempPath() + Guid.NewGuid().ToString() + ".ps1";
-                System.IO.File.WriteAllText(filePath, string.Join(" ", args));
-            }
-            else
-            {
-                filePath = System.IO.Path.Combine(Environment.CurrentDirectory, args[0]);
-            }
-
-            RunFile(filePath);
-        }
-
-        /// <summary>
-        /// Method to run the sample script and handle debugger events.
-        /// </summary>
-        public void RunFile(string filePath)
-        {
-            using (Runspace runspace = RunspaceFactory.CreateRunspace())
-            {
-                runspace.Open();
-                runspace.Debugger.SetDebugMode(DebugModes.LocalScript);
-
-                using (PowerShell powerShell = PowerShell.Create())
+                if (!File.Exists(fileName))
                 {
-                    powerShell.Runspace = runspace;
+                    return;
+                }
 
-                    runspace.Debugger.BreakpointUpdated += HandlerBreakpointUpdatedEvent;
-                    runspace.Debugger.DebuggerStop += HandleDebuggerStopEvent;
+                using (StreamReader r = new StreamReader(fileName))
+                {
+                    var jsonString = r.ReadToEnd();
+                    dynamic dynObj = JsonConvert.DeserializeObject(jsonString);
 
-                    Logger.Out("Starting script file: " + filePath);
-                    Logger.Out();
-                    powerShell.AddCommand("Set-PSBreakpoint").AddParameter("Script", filePath).AddParameter("Line", 1);
-                    powerShell.AddCommand("Set-ExecutionPolicy").AddParameter("ExecutionPolicy", "Bypass").AddParameter("Scope", "CurrentUser");
-                    powerShell.AddScript(filePath);
-
-                    var scriptOutput = new PSDataCollection<PSObject>();
-                    scriptOutput.DataAdded += (sender, args) =>
+                    foreach (var item in dynObj)
                     {
-                        // Stream script output to console.
-                        foreach (var item in scriptOutput.ReadAll())
-                        {
-                            Logger.Out("| " + item);
-                        }
-                    };
-
-                    powerShell.Invoke<PSObject>(null, scriptOutput);
-
-                    if (powerShell.Streams.Error != null)
-                    {
-                        foreach (ErrorRecord errorRecord in powerShell.Streams.Error.ReadAll())
-                        {
-                            if (errorRecord != null && errorRecord.ErrorDetails != null)
-                            {
-                                Logger.Out(errorRecord.ErrorDetails.Message);
-                            }
-                        }
+                        VariableReplaceMap[item.Path.ToString()] = item.Value.ToString();
                     }
                 }
             }
         }
-        #endregion
 
-        #region Private Methods
-        public void LoadConfig(string fileName)
+        // Method to handle the Debugger BreakpointUpdated event.
+        // This method will display the current breakpoint change and maintain a 
+        // collection of all current breakpoints.
+        private void HandlerBreakpointUpdatedEvent(object sender, BreakpointUpdatedEventArgs args)
         {
-            if (!File.Exists(fileName))
+            // Write message to console.
+            ConsoleColor saveFGColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Logger.Out();
+
+            switch (args.UpdateType)
             {
-                return;
+                case BreakpointUpdateType.Set:
+                    if (!BreakPoints.ContainsKey(args.Breakpoint.Id))
+                    {
+                        BreakPoints.Add(args.Breakpoint.Id, args.Breakpoint);
+                    }
+                    Logger.Out("HandlerBreakpointUpdatedEvent> breakpoint created");
+                    break;
+
+                case BreakpointUpdateType.Removed:
+                    BreakPoints.Remove(args.Breakpoint.Id);
+                    Logger.Out("HandlerBreakpointUpdatedEvent> breakpoint removed");
+                    break;
+
+                case BreakpointUpdateType.Enabled:
+                    Logger.Out("HandlerBreakpointUpdatedEvent> breakpoint enabled");
+                    break;
+
+                case BreakpointUpdateType.Disabled:
+                    Logger.Out("HandlerBreakpointUpdatedEvent> breakpoint disabled");
+                    break;
             }
 
-            using (StreamReader r = new StreamReader(fileName))
-            {
-                var jsonString = r.ReadToEnd();
-                dynamic dynObj = JsonConvert.DeserializeObject(jsonString);
-
-                foreach (var item in dynObj)
-                {
-                    VariableReplaceMap[item.Path.ToString()] = item.Value.ToString();
-                }
-            }
+            Logger.Out(args.Breakpoint.ToString());
+            Logger.Out();
+            Console.ForegroundColor = saveFGColor;
         }
 
         private Dictionary<string, object> VariableMap = new Dictionary<string, object>();
 
         private void UpdateVariableMap(Debugger debugger)
         {
-            var output = new PSDataCollection<PSObject>();
-            output.DataAdded += (dSender, dArgs) =>
+            var processVariables = new PSDataCollection<PSObject>();
+            processVariables.DataAdded += (dSender, dArgs) =>
             {
-                foreach (PSObject item in output.ReadAll())
+                foreach (PSObject item in processVariables.ReadAll())
                 {
                     if (item.ImmediateBaseObject is PSVariable)
                     {
@@ -135,8 +99,9 @@ namespace DebugPowerShell
                             Logger.Out(@"* " + psv.Name + ": " + psv.Value);
                         }
 
-                        if(VariableReplaceMap != null && VariableReplaceMap.ContainsKey(psv.Name))
+                        if (VariableReplaceMap != null && VariableReplaceMap.ContainsKey(psv.Name))
                         {
+                            Logger.Out(@"Updated variable " + psv.Name + ": " + psv.Value + " --> " + VariableReplaceMap[psv.Name]);
                             psv.Value = VariableReplaceMap[psv.Name];
                         }
 
@@ -147,7 +112,7 @@ namespace DebugPowerShell
                         ErrorRecord errorRecord = (ErrorRecord)item.ImmediateBaseObject;
 
                         Logger.Out(@"* item: " + item.ImmediateBaseObject.GetType());
-                        
+
                         if (errorRecord != null && errorRecord.ErrorDetails != null)
                         {
                             Logger.Out(errorRecord.ErrorDetails.Message);
@@ -162,7 +127,7 @@ namespace DebugPowerShell
 
             PSCommand psCommand = new PSCommand();
             psCommand.AddScript("Get-Variable");
-            DebuggerCommandResults results = debugger.ProcessCommand(psCommand, output);
+            DebuggerCommandResults results = debugger.ProcessCommand(psCommand, processVariables);
         }
 
         private DebuggerCommandResults RunCommand(Debugger debugger, string command)
@@ -177,9 +142,8 @@ namespace DebugPowerShell
             };
 
             PSCommand psCommand = new PSCommand();
-            psCommand.AddScript(command).AddCommand("Out-String").AddParameter("Stream", true);
+            psCommand.AddScript(command);
             DebuggerCommandResults results = debugger.ProcessCommand(psCommand, output);
-
             return results;
         }
 
@@ -200,11 +164,9 @@ namespace DebugPowerShell
         /// <param name="args">DebuggerStopEventArgs for current debugger stop</param>
         private void PrintDebuggerStopMessage(DebuggerStopEventArgs args)
         {
-            // Write debugger stop information in yellow.
             ConsoleColor saveFGColor = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.Yellow;
 
-            // Show help message only once.
             if (!ShowHelpMessage)
             {
                 Logger.Out("Entering debug mode. Type 'h' to get help.");
@@ -246,6 +208,14 @@ namespace DebugPowerShell
 
             UpdateVariableMap(debugger);
             PrintDebuggerStopMessage(args);
+            
+            foreach(Breakpoint breakpoint in args.Breakpoints)
+            {
+                if (BreakPoints.ContainsKey(breakpoint.Id))
+                {
+                    Logger.Out("Found breakpoint info");
+                }
+            }
 
             while (resumeAction == null)
             {
@@ -261,7 +231,7 @@ namespace DebugPowerShell
                     Logger.Out();
                     string[] commandArgs = commandLine.Split(' ');
 
-                    if (commandArgs.Length<=0)
+                    if (commandArgs.Length <= 0)
                     {
                         continue;
                     }
@@ -290,49 +260,76 @@ namespace DebugPowerShell
             //  - StepOut       Step out of function.
             //  - StepOver      Step over function.
             //  - Stop          Stop debugging.
-
-            Logger.Out("resumeAction.Value: " + resumeAction.Value);
             args.ResumeAction = resumeAction.Value;
         }
 
-        // Method to handle the Debugger BreakpointUpdated event.
-        // This method will display the current breakpoint change and maintain a 
-        // collection of all current breakpoints.
-        private void HandlerBreakpointUpdatedEvent(object sender, BreakpointUpdatedEventArgs args)
+        /// <summary>
+        /// Method to run the sample script and handle debugger events.
+        /// </summary>
+        public void RunFile(string filePath)
         {
-            // Write message to console.
-            ConsoleColor saveFGColor = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Logger.Out();
-
-            switch (args.UpdateType)
+            using (Runspace runspace = RunspaceFactory.CreateRunspace())
             {
-                case BreakpointUpdateType.Set:
-                    if (!BreakPoints.ContainsKey(args.Breakpoint.Id))
+                runspace.Open();
+                runspace.Debugger.SetDebugMode(DebugModes.LocalScript);
+
+                using (PowerShell powerShell = PowerShell.Create())
+                {
+                    powerShell.Runspace = runspace;
+
+                    runspace.Debugger.BreakpointUpdated += HandlerBreakpointUpdatedEvent;
+                    runspace.Debugger.DebuggerStop += HandleDebuggerStopEvent;
+
+                    Logger.Out("Starting script file: " + filePath);
+                    powerShell.AddStatement().AddCommand("Set-PSBreakpoint").AddParameter("Script", filePath).AddParameter("Line", 1);
+                    foreach (var kv in VariableReplaceMap)
                     {
-                        BreakPoints.Add(args.Breakpoint.Id, args.Breakpoint);
+                        Logger.Out("Setting variable breakpoint on: " + kv.Key);
+                        powerShell.AddStatement().AddCommand("Set-PSBreakpoint").AddParameter("Variable", kv.Key);
                     }
-                    Logger.Out("Breakpoint created:");
-                    break;
+                    powerShell.AddStatement().AddCommand("Set-ExecutionPolicy").AddParameter("ExecutionPolicy", "Bypass").AddParameter("Scope", "CurrentUser");
+                    powerShell.AddScript(filePath);
 
-                case BreakpointUpdateType.Removed:
-                    BreakPoints.Remove(args.Breakpoint.Id);
-                    Logger.Out("Breakpoint removed:");
-                    break;
+                    var scriptOutput = new PSDataCollection<PSObject>();
+                    scriptOutput.DataAdded += (sender, args) =>
+                    {
+                        foreach (var item in scriptOutput.ReadAll())
+                        {
+                            Logger.Out("| " + item);
+                        }
+                    };
 
-                case BreakpointUpdateType.Enabled:
-                    Logger.Out("Breakpoint enabled:");
-                    break;
+                    powerShell.Invoke<PSObject>(null, scriptOutput);
 
-                case BreakpointUpdateType.Disabled:
-                    Logger.Out("Breakpoint disabled:");
-                    break;
+                    if (powerShell.Streams.Error != null)
+                    {
+                        foreach (ErrorRecord errorRecord in powerShell.Streams.Error.ReadAll())
+                        {
+                            if (errorRecord != null && errorRecord.ErrorDetails != null)
+                            {
+                                Logger.Out(errorRecord.ErrorDetails.Message);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void RunCode(string[] args)
+        {
+            string filePath = string.Empty;
+
+            if (!System.IO.File.Exists(args[0]))
+            {
+                filePath = System.IO.Path.GetTempPath() + Guid.NewGuid().ToString() + ".ps1";
+                System.IO.File.WriteAllText(filePath, string.Join(" ", args));
+            }
+            else
+            {
+                filePath = System.IO.Path.Combine(Environment.CurrentDirectory, args[0]);
             }
 
-            Logger.Out(args.Breakpoint.ToString());
-            Logger.Out();
-            Console.ForegroundColor = saveFGColor;
+            RunFile(filePath);
         }
-        #endregion
     }
 }
